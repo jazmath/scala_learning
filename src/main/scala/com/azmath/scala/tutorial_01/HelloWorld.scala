60eb46f0dd6b7dd3f6a183e9462495b0cadf719d
Azmath Ikram <azmath.ikram1989@gmail.com>
2018-09-14 16:44:23 +0530

    initial commit

---
+++ src/main/scala/com/azmath/scala/tutorial_01/HelloWorld.scala
@@ -0,0 +1,6 @@
+package com.azmath.scala.tutorial_01
+
+object HelloWorld extends App {
+
+  println("Hello Scala World");
+}
---
+++ src/main/scala/com/azmath/scala/tutorial_02/ScalaMain.scala
@@ -0,0 +1,10 @@
+package com.azmath.scala.tutorial_02
+
+object ScalaMain {
+
+  def main(args : Array[String]) : Unit = {
+      println("Hello world from the main.")
+  }
+
+  println("This is out side of the main")
+}
